1d00c9ff3b36cebe3fd6f70ca78d7dd422b2c4a9
Dreampie <Dreampie@outlook.com>
2015-12-11 17:53:32 +0800

    add save for repository

--- user-service/user-service-provider/src/main/java/cn/dreampie/service/user/repository/UserRepository.java
+++ user-service/user-service-provider/src/main/java/cn/dreampie/service/user/repository/UserRepository.java
@@ -8,6 +8,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  */
 public interface UserRepository extends MongoRepository<UserEntity, String> {
   UserEntity findById(String id);
-
-  UserEntity save(UserEntity user);
 }
